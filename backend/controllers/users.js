ec3ceb5b425191de23c2dfac076d38e7a511643e
AngeliqueMarachev <angeliquealive@gmail.com>
2022-10-13 11:18:44 +0300

    Create login controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -92,10 +92,22 @@ const updateAvatar = (req, res) => {
   return updateUserData(req, res);
 };
 
+const login = (req, res) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials({ email, password })
+    .then((user) => {
+
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
+
 module.exports = {
   getUsers,
   getUser,
   createUser,
   updateUser,
   updateAvatar,
+  login,
 };
