55498629e579f3946c32d36cb141a1ea9ea15999
AngeliqueMarachev <angeliquealive@gmail.com>
2022-10-16 14:01:24 +0300

    Add Celebrate Validation

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getCards,
@@ -9,9 +10,72 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+
+router.post(
+  '/cards',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        name: Joi.string().required().min(1).max(30),
+        link: Joi.string().uri().required(),
+        user: Joi.object().keys({
+          _id: Joi.string().alphanum().required(),
+        }).unknown(true),
+      })
+      .unknown(true),
+  }),
+  createCard,
+);
+
+router.delete(
+  '/cards/:cardId',
+  celebrate({
+    body: Joi.object().keys({
+      user: Joi.object().keys({
+        _id: Joi.string().alphanum().required(),
+      }).unknown(true),
+    }).unknown(true),
+    params: Joi.object()
+      .keys({
+        cardId: Joi.string().alphanum().required(),
+      })
+      .unknown(true),
+  }),
+  deleteCard,
+);
+
+router.put(
+    '/cards/:cardId/likes',
+    celebrate({
+      body: Joi.object().keys({
+        user: Joi.object().keys({
+          _id: Joi.string().alphanum().required(),
+        }).unknown(true),
+      }).unknown(true),
+      params: Joi.object()
+        .keys({
+          cardId: Joi.string().alphanum().required(),
+        })
+        .unknown(true),
+    }),
+    likeCard,
+  );
+
+router.delete(
+  '/cards/:cardId/likes',
+  celebrate({
+    body: Joi.object().keys({
+      user: Joi.object().keys({
+        _id: Joi.string().alphanum().required(),
+      }).unknown(true),
+    }).unknown(true),
+    params: Joi.object()
+      .keys({
+        cardId: Joi.string().alphanum().required(),
+      })
+      .unknown(true),
+  }),
+  dislikeCard,
+);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,18 +1,66 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
+
 const {
   getUsers,
   getUser,
-  // createUser,
   updateUser,
   updateAvatar,
   getCurrentUser,
-} = require('../controllers/users');
+} = require("../controllers/users");
 
-router.get('/users', getUsers);
-router.get('/users/:userId', getUser);
-// router.post('/users', createUser);
-router.get('/users/me', getCurrentUser);
-router.patch('/users/me', updateUser);
-router.patch('/users/me/avatar', updateAvatar);
+router.get("/users", getUsers);
+router.get("/users/:userId", getUser);
+
+router.get(
+  "/users/me",
+  celebrate({
+    body: Joi.object()
+      .keys({
+        user: Joi.object()
+          .keys({
+            _id: Joi.string().alphanum().required(),
+          })
+          .unknown(true),
+      })
+      .unknown(true),
+  }),
+  getCurrentUser
+);
+
+router.patch(
+  "/users/me",
+  celebrate({
+    body: Joi.object()
+      .keys({
+        user: Joi.object()
+          .keys({
+            _id: Joi.string().alphanum().required(),
+          })
+          .unknown(true),
+        name: Joi.string().min(2).max(40),
+        about: Joi.string().min(2).max(200),
+      })
+      .unknown(true),
+  }),
+  updateUser
+);
+
+router.patch(
+  "/users/me/avatar",
+  celebrate({
+    body: Joi.object()
+      .keys({
+        user: Joi.object()
+          .keys({
+            _id: Joi.string().alphanum().required(),
+          })
+          .unknown(true),
+        link: Joi.string().uri().required(),
+      })
+      .unknown(true),
+  }),
+  updateAvatar
+);
 
 module.exports = router;
